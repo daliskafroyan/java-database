696d969ca6c8834fbf0c4a21b8f699ba848f9ec8
firstaroyand <firstaroyan09@gmail.com>
2023-02-19 22:37:28 +0700

    fix: rename package

--- JavaDatabase.java
+++ JavaDatabase.java
@@ -1,7 +1,7 @@
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-package JavaDatabase;
+package internal.database;
 
 public class JavaDatabase {
     public static void main(String[] args) {
